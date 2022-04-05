3e51c5fa28ed95a38b4b7bc2e072ce3d4839fa73
Joshua Ani <josh4820@gmail.com>
2022-04-05 12:58:23 -0400

    Added tests for Value<f32>-to-shader conversion

--- src/lib.rs
+++ src/lib.rs
@@ -276,4 +276,12 @@ mod tests {
         let s = Vec4::new(1., 2., -1., 2.).to_wgsl_string();
         assert_eq!(s, "vec4<f32>(1., 2., -1., 2.)");
     }
+
+    #[test]
+    fn to_wgsl_value_f32() {
+        let s = Value::Single(1.0_f32).to_wgsl_string();
+        assert_eq!(s, "1.");
+        let s = Value::Uniform((1.0_f32, 2.0_f32)).to_wgsl_string();
+        assert_eq!(s, "rand() * (2. - 1.) + 1.");
+    }
 }
