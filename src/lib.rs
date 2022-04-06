0e6e4abc2584d756dd87740c7425f533173eff5a
eliotbo <eliot.bolduc@gmail.com>
2022-04-06 15:33:49 -0400

    deleted orbit.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -85,12 +85,9 @@ mod asset;
 mod bundle;
 mod gradient;
 mod modifiers;
-mod orbit;
 mod plugin;
 mod render;
 mod spawn;
-
-pub use orbit::*;
 
 pub use asset::EffectAsset;
 pub use bundle::ParticleEffectBundle;
