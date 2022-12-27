34215af34af7852342211667bee9c5e99cb24736
Jerome Humbert <djeedai@gmail.com>
2022-12-27 09:56:57 +0000

    Add examples to `run_examples.bat` and `Cargo.toml` (#114)
    Add some missing example to `run_examples.bat`, and all the missing
    examples to `Cargo.toml`.

--- run_examples.bat
+++ run_examples.bat
@@ -13,5 +13,6 @@ cargo r --example instancing --no-default-features --features="bevy/bevy_winit b
 REM 3D + PNG
 cargo r --example gradient --no-default-features --features="bevy/bevy_winit bevy/bevy_pbr bevy/png 3d"
 cargo r --example circle --no-default-features --features="bevy/bevy_winit bevy/bevy_pbr bevy/png 3d"
+cargo r --example billboard --no-default-features --features="bevy/bevy_winit bevy/bevy_pbr bevy/png 3d"
 REM 2D
 cargo r --example 2d --no-default-features --features="bevy/bevy_winit bevy/bevy_sprite 2d"
