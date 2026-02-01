104a2fa1509d933db9cf0228561acbafc0910842
Jerome Humbert <djeedai@gmail.com>
2026-02-01 12:15:59 +0000

    Bump tarpaulin version

--- build_examples_wasm.bat
+++ build_examples_wasm.bat
@@ -1,6 +1,6 @@
 @echo on
 
-echo Check that wasm-bindgen -V returns 0.2.104, otherwise cargo install wasm-bindgen-cli --version 0.2.104
+echo Check that wasm-bindgen -V returns 0.2.108, otherwise cargo install wasm-bindgen-cli --version 0.2.108
 
 echo Setting RUSTFLAGS to enable unstable web_sys APIs...
 set RUSTFLAGS=--cfg web_sys_unstable_apis --cfg getrandom_backend="wasm_js"
