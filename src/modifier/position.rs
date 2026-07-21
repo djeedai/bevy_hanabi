5c995128225996c11dfe69e5463524ab79a8b6ff
Lucas Farias <lucasfnf@gmail.com>
2026-07-21 12:07:41 -0300

    SetPositionBoxModifier

--- src/modifier/position.rs
+++ src/modifier/position.rs
@@ -343,3 +343,125 @@ impl Modifier for SetPositionCone3dModifier {
         Ok(())
     }
 }
+
+/// A modifier to set the position of particles on a box.
+///
+/// The box is axis aligned.
+///
+/// Particles are moved somewhere inside the volume or on the surface of a
+/// box define by its scale.
+///
+/// # Attributes
+///
+/// This modifier requires the following particle attributes:
+/// - [`Attribute::POSITION`]
+#[derive(Clone, Copy, Hash, Reflect)]
+pub struct SetPositionBoxModifier {
+    /// The scale of the box.
+    ///
+    /// Expression type: `Vec3`
+    pub scale: ExprHandle,
+    /// The shape dimension to set the position to.
+    pub dimension: ShapeDimension,
+}
+
+impl SetPositionBoxModifier {
+    fn eval(
+        &self,
+        module: &mut Module,
+        context: &mut dyn EvalContext,
+    ) -> Result<String, ExprError> {
+        let func_id = calc_func_id(self);
+        let func_name = format!("set_position_box_{0:016X}", func_id);
+
+        context.make_fn(
+            &func_name,
+            "particle: ptr<function, Particle>",
+            module,
+            &mut |m: &mut Module, ctx: &mut dyn EvalContext| -> Result<String, ExprError> {
+                let scale = ctx.eval(m, self.scale)?;
+
+                let code = match self.dimension {
+                    ShapeDimension::Surface => {
+                        format!(
+                            r#"    let scale = {};
+
+    let face = frand();
+    let rand1 = frand() - 0.5;
+    let rand2 = frand() - 0.5;
+    let fixed = 0.5;
+
+    var x: f32;
+    var y: f32;
+    var z: f32;
+
+    if face < (1. / 6.) {{
+        x = rand1 * scale.x;
+        y = fixed * scale.y;
+        z = rand2 * scale.z;
+    }} else if face < (2. / 6.) {{
+        x = rand1 * scale.x;
+        y = -fixed * scale.y;
+        z = rand2 * scale.z;
+    }} else if face < (3. / 6.) {{
+        x = fixed * scale.x;
+        y = rand1 * scale.y;
+        z = rand2 * scale.z;
+    }} else if face < (4. / 6.) {{
+        x = -fixed * scale.x;
+        y = rand1 * scale.y;
+        z = rand2 * scale.z;
+    }} else if face < (5. / 6.) {{
+        x = rand1 * scale.x;
+        y = rand2 * scale.y;
+        z = fixed * scale.z;
+    }} else {{
+        x = rand1 * scale.x;
+        y = rand2 * scale.y;
+        z = -fixed * scale.z;
+    }}
+    (*particle).{} = vec3(x, y, z);
+"#,
+                            scale,
+                            Attribute::POSITION.name()
+                        )
+                    }
+                    ShapeDimension::Volume => format!(
+                        r#"    let scale = {};
+    let mult = vec3(frand(), frand(), frand()) - 0.5;
+    (*particle).{} = scale * mult;
+"#,
+                        scale,
+                        Attribute::POSITION.name()
+                    ),
+                };
+
+                Ok(code)
+            },
+        )?;
+
+        let code = format!("{}(&particle);\n", func_name);
+
+        Ok(code)
+    }
+}
+
+impl Modifier for SetPositionBoxModifier {
+    fn context(&self) -> ModifierContext {
+        ModifierContext::Init | ModifierContext::Update
+    }
+
+    fn attributes(&self) -> &[Attribute] {
+        &[Attribute::POSITION]
+    }
+
+    fn boxed_clone(&self) -> BoxedModifier {
+        Box::new(*self)
+    }
+
+    fn apply(&self, module: &mut Module, context: &mut ShaderWriter) -> Result<(), ExprError> {
+        let code = self.eval(module, context)?;
+        context.main_code += &code;
+        Ok(())
+    }
+}
